63040b42c0d64d9a5bf52bb0ece4086d380ad4eb
Matheus Correia <matheuscorreia1910@gmail.com>
2026-06-02 17:23:15 -0300

    deploy frontend

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,5 +1,13 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:3001",
+// });
+
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  // Troca o localhost fixo pela variável que criamos no .env.local
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
