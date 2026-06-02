fbc850b19a2ad2cbb91db0384c5ba39815c929f6
Matheus Correia <matheuscorreia1910@gmail.com>
2026-06-02 17:39:57 -0300

    fix: adjust import path for deployment

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,33 @@
-import ClientesTable from "@/components/ClientesTable";
+// import ClientesTable from "@/components/ClientesTable";
+
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen bg-gray-100 p-10">
+//       <div className="max-w-6xl mx-auto">
+
+//         <h1 className="text-5xl font-bold mb-10">
+//           Dashboard Empresa
+//         </h1>
+
+//         <ClientesTable />
+
+//       </div>
+//     </main>
+//   );
+// }
+
+
+// Volte uma pasta para sair de 'app' e entre em 'components'
+import ClientesTable from "../components/ClientesTable";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 p-10">
       <div className="max-w-6xl mx-auto">
-
         <h1 className="text-5xl font-bold mb-10">
           Dashboard Empresa
         </h1>
-
         <ClientesTable />
-
       </div>
     </main>
   );
